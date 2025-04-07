87f6a90eb30e4bfeda3b185c265aba1be961bc61
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-08 03:19:34 +0530

    NJ ep6

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 var a=10;
-function b(){
-    var x=10;
-}
+//not agood practice
+a=undefined;
 
-//all three lines below do the same work
-console.log(window.a);
-console.log(a);
-console.log(this.a);
+//loosely typed language
+a='hello';
 
