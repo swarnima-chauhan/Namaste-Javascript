7e060907bf471bd2e18c27d7c0361b71c5e06654
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-20 01:38:31 +0530

    episode 2

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-console.log("Namaste JS");
+const cart=["shoes","pants","kurta"];
 
-console.log("Season 2");
+//CallBack Hell
+createOrder(cart,function(orderId){
+    proceedToPayment(orderId,function(paymentInfo){
+        showOrderSummary(paymentInfo,function(){
+            updateWalletBalance();
+        });
+    });
+});
+
+
+//Promises chain
+createOrder(cart).
+then(function(orderId){
+    return proceedToPayment(orderId);
+})
+.then(function(paymentInfo){
+    return showOrderSummary(paymentInfo);
+})
+.then(function(paymentInfo){
+    updateWalletBalance(paymentInfo);
+}); 
+   
+
+
 
