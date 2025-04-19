149c3611c5977bf3959bcc195a05e315f13313b1
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-20 02:46:53 +0530

    ep 3 done

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const cart=["shoes","pants","kurta"];
 
-//CallBack Hell
-createOrder(cart,function(orderId){
-    proceedToPayment(orderId,function(paymentInfo){
-        showOrderSummary(paymentInfo,function(){
-            updateWalletBalance();
-        });
-    });
-});
-
-
-//Promises chain
-createOrder(cart).
-then(function(orderId){
+createOrder(cart)
+  .then(function(orderId){
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function(orderId){
     return proceedToPayment(orderId);
-})
-.then(function(paymentInfo){
-    return showOrderSummary(paymentInfo);
-})
-.then(function(paymentInfo){
-    updateWalletBalance(paymentInfo);
-}); 
+  })
+  .then(function(paymentInfo){
+    console.log(paymentInfo);
+  })
+  .catch(function(err){
+    console.log(err.message);
+  });
    
+///Producer End
 
+function createOrder(cart){
+    const pr=new Promise(function(resolve,reject){
+        //create an order
+        //validate cart
+        //orderId
+        if(!validateCart(cart)){
+            const err=new Error("Cart is not valid");
+            reject(err);
+        }
+        //logic for create order
+        const orderId="12345";
+        if(orderId){
+            resolve(orderId);
+        }
+    });
+    return pr;
+}
 
+function validateCart(cart){
+    return true;
+}
+
+function proceedToPayment(orderId){
+    return new Promise(function(resolve,reject){
+        resolve("Payment successful");
+    });
+}
 
