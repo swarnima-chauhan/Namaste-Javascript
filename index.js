80c88803df590f4f903a976e8f1ad8788f0e5461
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-21 00:54:55 +0530

    call aplly bind

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const p1=new Promise((resolve,reject)=>{
-    //setTimeout(()=>resolve("P1 success"),3000);
-    setTimeout(()=>reject("P1 fails"),3000);
-})
+const student={
+    name:"swarnima",
+    printName:function(){
+        console.log(this.name);
+    }
+}
 
-const p2=new Promise((resolve,reject)=>{
-    //setTimeout(()=>resolve("P2 success"),1000);
-    setTimeout(()=>reject("P2 fails"),1000);
-})
+const student2={
+    name:"akshay"
+}
 
-const p3=new Promise((resolve,reject)=>{
-    //setTimeout(()=>resolve("P3 success"),2000);
-    setTimeout(()=>reject("P3 fails"),2000);
-})
 
-Promise.any([p1,p2,p3])
-.then(res=>console.log(res))
-.catch(err=>{
-    console.error(err);
-    console.log(err.errors);
-});
+student.printName.call(student2);
+student.printName.apply(student2);
+let students= student.printName.bind(student2);
+console.log(students);
+students();
